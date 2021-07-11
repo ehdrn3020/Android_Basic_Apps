ae8cac4d96e2e974b3c6440d608d84cf9ccaa146
DongGu Kang <ehdrnsla3020@naver.com>
2021-07-11 22:15:50 +0900

    01.BMI_Calculator - Create Layout

---
+++ 01.BMI_Calculator/app/src/main/java/com/example/bmi_calculator/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.bmi_calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val heightEditText : EditText = findViewById(R.id.heightEditText)
+        val weightEditText = findViewById<EditText>(R.id.weightEditText)
+        val resultButton = findViewById<Button>(R.id.resultButton)
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"안드로이드 App개발 with Kotlin Basic" 
